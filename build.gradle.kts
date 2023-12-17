e02097d75eae5cc10c537f99ca17f43f16c2e33f
LoaTcHi <francoisturcan31@gmail.com>
2023-12-17 19:41:00 +0100

    Added misc gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    application
+}
+
+group = "bomberman.algo2"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.6.2")
+    implementation("com.github.ajalt.clikt:clikt:4.2.1")
+    implementation("org.jline:jline:3.24.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.jar { // could also be a new task rather than the default one
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
